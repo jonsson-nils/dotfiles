2a5af99cee4dd4da868c70d3a467eaa2153ecb73
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2022-03-05 23:01:01 +0000

    Added Azure module.

--- pwsh/install.ps1
+++ pwsh/install.ps1
@@ -1,3 +1,4 @@
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module posh-git -Scope CurrentUser -Repository PSGallery
 Install-Module oh-my-posh -Scope CurrentUser -Repository PSGallery
+Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
