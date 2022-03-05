de52b1eb7e69e03c5f11755d87a14f05a3cf6cca
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2022-03-05 03:20:47 +0000

    Removed deprecated configuration.

--- install.sh
+++ install.sh
@@ -10,25 +10,4 @@ install_zsh_config() {
   ln -sf "$DOTFILES_HOME/src/zsh/"* "$HOME/.oh-my-zsh/custom/"
 }
 
-install_tabnine_config() {
-  if [ ! -s "$HOME/.config/TabNine" ]; then
-    echo "Installing TabNine configuration."
-
-    rm -rf "$HOME/.config/TabNine" && \
-    mkdir -p "$HOME/.config" && \
-    ln -sf "$DOTFILES_HOME/src/TabNine" "$HOME/.config/TabNine"
-  fi
-}
-
-install_docker_config() {
-  if [ ! -d "$HOME/.docker/" ]; then
-    echo "Installing Docker configuration."
-
-    mkdir -p "$HOME/.docker" && \
-    cp -R "$DOTFILES_HOME/src/docker/config.json" "$HOME/.docker/config.json"
-  fi
-}
-
-# install_tabnine_config
-# install_docker_config
 install_zsh_config
