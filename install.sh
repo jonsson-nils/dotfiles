6eeac506451fb603e14762e4a5c16a594570901c
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2022-03-05 03:38:17 +0000

    (WIP) Powershell Configuration

--- install.sh
+++ install.sh
@@ -10,4 +10,8 @@ ln -sf "$DOTFILES_HOME/zsh/"* "$HOME/.oh-my-zsh/custom/"
 # Installing PWSH configuration
 pwsh install.ps1
 mkdir -p "$HOME"/.config/powershell
-ln -sf "$DOTFILES_HOME"/.dotfiles/pwsh/profile.ps1 "$HOME"/.config/powershell/profile.ps1
+cp "$DOTFILES_HOME"/.dotfiles/pwsh/profile.ps1 "$HOME"/.config/powershell/profile.ps1
+
+# Configure Git
+git config --global core.editor "code"
+git config --global pull.ff only
