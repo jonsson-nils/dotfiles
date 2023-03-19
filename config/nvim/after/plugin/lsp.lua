ae36b2929e96ba4144292ba5156564543931dc35
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-03-19 22:56:01 +0100

    Fixed configuration for rust-analyzer.

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -28,7 +28,7 @@ lsp.set_preferences({
   sign_icons = { }
 })
 
-lsp.on_attach(function(client, bufnr)
+local on_attach = function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -40,7 +40,30 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
+end
 
+lsp.configure('rust_analyzer', {
+  on_attach = on_attach,
+  settings = {
+    ["rust_analyzer"] = {
+      imports = {
+        granularity = {
+          group = "module",
+        },
+        prefix = "self",
+      },
+      cargo = {
+        buildScripts = {
+          enable = true,
+        },
+      },
+      procMacro = {
+        enable = true,
+      },
+    }
+  }
+})
+
+lsp.on_attach(on_attach)
 lsp.setup()
 
