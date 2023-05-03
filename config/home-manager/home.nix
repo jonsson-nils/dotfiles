afa6a1ac8012c3310231e84c9d86f0b58b6ecdeb
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-05-04 01:42:01 +0200

    Added neotree.

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -20,6 +20,7 @@
     ".config/nushell/config.nu".source                = ~/projects/dotfiles/config/nushell/config.nu;
     ".config/nushell/env.nu".source                   = ~/projects/dotfiles/config/nushell/env.nu;
     ".config/nvim/after/plugin/bufferline.lua".source = ~/projects/dotfiles/config/nvim/after/plugin/bufferline.lua;
+    ".config/nvim/after/plugin/neotree.lua".source    = ~/projects/dotfiles/config/nvim/after/plugin/neotree.lua;
     ".config/nvim/after/plugin/fugitive.lua".source   = ~/projects/dotfiles/config/nvim/after/plugin/fugitive.lua;
     ".config/nvim/after/plugin/git-gutter.lua".source = ~/projects/dotfiles/config/nvim/after/plugin/git-gutter.lua;
     ".config/nvim/after/plugin/harpoon.lua".source    = ~/projects/dotfiles/config/nvim/after/plugin/harpoon.lua;
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -138,6 +138,17 @@ return require('packer').startup(function(use)
 
   use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
 
+  vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
+
   if packer_bootstrap then
     require('packer').sync()
   end
