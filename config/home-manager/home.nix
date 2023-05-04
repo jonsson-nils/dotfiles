cc4e0c726bbfaa5fa55907b2fd518569bb3938e1
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-05-04 04:08:07 +0200

    Fixed setup with copilot.

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -20,6 +20,7 @@
     ".config/nushell/config.nu".source                = ~/projects/dotfiles/config/nushell/config.nu;
     ".config/nushell/env.nu".source                   = ~/projects/dotfiles/config/nushell/env.nu;
     ".config/nvim/after/plugin/bufferline.lua".source = ~/projects/dotfiles/config/nvim/after/plugin/bufferline.lua;
+    ".config/nvim/after/plugin/copilot.lua".source = ~/projects/dotfiles/config/nvim/after/plugin/copilot.lua;
     ".config/nvim/after/plugin/neotree.lua".source    = ~/projects/dotfiles/config/nvim/after/plugin/neotree.lua;
     ".config/nvim/after/plugin/neogit.lua".source    = ~/projects/dotfiles/config/nvim/after/plugin/neogit.lua;
     ".config/nvim/after/plugin/fugitive.lua".source   = ~/projects/dotfiles/config/nvim/after/plugin/fugitive.lua;
--- config/nvim/after/plugin/copilot.lua
+++ config/nvim/after/plugin/copilot.lua
@@ -1,5 +1,4 @@
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
-vim.api.nvim_set_keymap("i", "<CR>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<C-Space>", 'copilot#Complete()', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-u>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
