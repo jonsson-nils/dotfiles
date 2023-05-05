15334dcb7b4a8c2d9ca2baded4c35718098c0f1b
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-05-05 18:48:09 +0200

    Changed copilot to use <C-a>.

--- config/nvim/after/plugin/copilot.lua
+++ config/nvim/after/plugin/copilot.lua
@@ -1,4 +1,4 @@
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
-vim.api.nvim_set_keymap("i", "<C-u>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
