438e2f99fea4d0e8d646b5c7174d5885a4b688f6
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-05-06 22:45:50 +0200

    Cleaned up keymaps.

--- config/nvim/lua/user/keymaps.lua
+++ config/nvim/lua/user/keymaps.lua
@@ -1,35 +1,21 @@
---vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- disable bad things
+vim.keymap.set("n", "Q", "<nop>")
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
-vim.keymap.set("x", "<leader>p", "\"_dP")
-
+-- system clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>p", "\"+p")
 vim.keymap.set("v", "<leader>p", "\"+p")
-
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("x", "<leader>p", "\"_dP")
 
-vim.keymap.set("n", "Q", "<nop>")
+-- todo: finish this
+-- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- vim.keymap.set("n", "<leader>x", "<cmd>!chmod u+x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
---vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
---vim.keymap.set("n", "<leader>x", "<cmd>!chmod u+x %<CR>", { silent = true })
-
+-- window management
 vim.keymap.set("n", "<leader>wj", "<C-w>j")
 vim.keymap.set("n", "<leader>wk", "<C-w>k")
 vim.keymap.set("n", "<leader>wh", "<C-w>h")
@@ -41,20 +27,33 @@ vim.keymap.set('n', '<leader>ww', '<C-w>w')
 vim.keymap.set('n', '<leader>wx', '<C-w>x')
 vim.keymap.set("n", "<leader>w=", "<C-w>=")
 
+-- buffer managment
 vim.keymap.set('n', '<leader>bq', [[:bdelete<CR>]])
 vim.keymap.set('n', '<leader>bw', [[:w<CR>]])
 vim.keymap.set('n', '<leader>bx', [[:w<CR>:bdelete<CR>]])
 vim.keymap.set('n', '<leader>bl', [[:bnext<CR>]])
 vim.keymap.set('n', '<leader>bh', [[:bprevious<CR>]])
 
+-- navigation
+--vim.keymap.set('n', 'j', 'jzz', { noremap = true, silent = true })
+--vim.keymap.set('v', 'j', 'jzz', { noremap = true, silent = true })
+--vim.keymap.set('n', 'k', 'kzz', { noremap = true, silent = true })
+--vim.keymap.set('v', 'k', 'kzz', { noremap = true, silent = true })
+vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "gs", "^")
 vim.keymap.set("v", "gs", "^")
 vim.keymap.set("n", "gl", "$")
 vim.keymap.set("v", "gl", "$")
 vim.keymap.set("n", "gh", "0")
 vim.keymap.set("v", "gh", "0")
-vim.keymap.set("n", "ge", "G")
-vim.keymap.set("v", "ge", "G")
+vim.keymap.set("n", "ge", "Gzz")
+vim.keymap.set("v", "ge", "Gzz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 --vim.keymap.set("n", "<leader><leader>", function()
 --  vim.cmd("so")
