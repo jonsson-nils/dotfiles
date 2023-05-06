d6215ff0b809d2e45be62e26e312fcb5facb6131
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-05-06 23:37:53 +0200

    Further adjustments.

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -13,20 +13,14 @@ telescope.load_extension("refactoring")
 telescope.load_extension("git_worktree")
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>tg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>tf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ta', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ts', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>tg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>t?', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>ts', function()
+vim.keymap.set('n', '<leader>tS', function()
   builtin.grep_string({ search = vim.fn.input("grep > ") })
 end)
-vim.api.nvim_set_keymap(
-	"v",
-	"<leader>tr",
-	"<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
-	{ noremap = true }
-)
+vim.keymap.set("v", "<leader>tr", telescope.extensions.refactoring.refactors)
 vim.keymap.set('n', '<leader>two', telescope.extensions.git_worktree.git_worktrees)
 vim.keymap.set('n', '<leader>twc', telescope.extensions.git_worktree.create_git_worktree)
-
