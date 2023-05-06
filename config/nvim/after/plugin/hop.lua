d911a5c686f25b2c9161d684f42a1cf8c4840417
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-05-06 17:28:28 +0200

    Moved hop to <leader>j prefix.

--- config/nvim/after/plugin/hop.lua
+++ config/nvim/after/plugin/hop.lua
@@ -2,5 +2,6 @@ local hop = require('hop')
 
 hop.setup { keys = 'etovxqpdygfblzhckisuran' }
 
-vim.keymap.set('n', '<leader>hh', hop.hint_words)
+vim.keymap.set('n', '<leader>jj', hop.hint_words)
+vim.keymap.set('v', '<leader>jj', hop.hint_words)
 
