bb71d9e46a72a5b5bd3b5e00344e13b9b9416d76
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-05-06 03:32:10 +0200

    Installed neorg.

--- config/nvim/lua/user/packer.lua
+++ config/nvim/lua/user/packer.lua
@@ -89,6 +89,26 @@ return require('packer').startup(function(use)
     'sindrets/diffview.nvim',
     requires = 'nvim-lua/plenary.nvim',
   }
+  use {
+    "nvim-neorg/neorg",
+    config = function()
+      require('neorg').setup {
+        load = {
+          ["core.defaults"] = {},        -- Loads default behaviour
+          ["core.concealer"] = {},       -- Adds pretty icons to your documents
+          ["core.dirman"] = {            -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        },
+      }
+    end,
+    run = ":Neorg sync-parsers",
+    requires = "nvim-lua/plenary.nvim",
+  }
 
   if packer_bootstrap then
     require('packer').sync()
