2f39b9c36760e90f87a0b2aa854d38d60540461c
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-05-06 17:39:06 +0200

    Changed prefix for LSP hotkeys.

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -20,15 +20,15 @@ lsp.set_preferences({
   sign_icons = {}
 })
 
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   local opts = { buffer = bufnr, remap = false }
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end)
   vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end)
   vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("n", "<leader>cf", function() vim.lsp.buf.format() end)
-  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end)
-  vim.keymap.set("n", "<leader>ch", function() vim.lsp.buf.hover() end)
+  vim.keymap.set("n", "<leader>.f", function() vim.lsp.buf.format() end)
+  vim.keymap.set("n", "<leader>.a", function() vim.lsp.buf.code_action() end)
+  vim.keymap.set("n", "<leader>.h", function() vim.lsp.buf.hover() end)
   vim.keymap.set("n", "<leader>s", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
