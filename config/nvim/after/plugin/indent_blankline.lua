92ec1a1863ffff8eb7e967a19d86d54899d41367
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-05-08 01:28:38 +0200

    Fixed some stuff.

--- config/nvim/after/plugin/indent_blankline.lua
+++ config/nvim/after/plugin/indent_blankline.lua
@@ -2,14 +2,6 @@ require("indent_blankline").setup {
   -- for example, context is off by default, use this to turn it on
   show_current_context = true,
   show_current_context_start = false,
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-    "IndentBlanklineIndent2",
-    "IndentBlanklineIndent3",
-    "IndentBlanklineIndent4",
-    "IndentBlanklineIndent5",
-    "IndentBlanklineIndent6",
-  },
 }
 
 vim.cmd [[highlight IndentBlanklineContextChar guifg=#FFFFFF gui=nocombine]]
