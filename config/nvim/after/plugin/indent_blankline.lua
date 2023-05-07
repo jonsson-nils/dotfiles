eae101d8b8e22b1afec913bc5f3f02b0157e3a34
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-05-08 00:58:30 +0200

    Changed highlighting of indent_blankline.

--- config/nvim/after/plugin/indent_blankline.lua
+++ config/nvim/after/plugin/indent_blankline.lua
@@ -1,11 +1,3 @@
-vim.cmd [[highlight IndentBlanklineContextChar guifg=#FFFFFF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-
 require("indent_blankline").setup {
   -- for example, context is off by default, use this to turn it on
   show_current_context = true,
@@ -19,3 +11,5 @@ require("indent_blankline").setup {
     "IndentBlanklineIndent6",
   },
 }
+
+vim.cmd [[highlight IndentBlanklineContextChar guifg=#FFFFFF gui=nocombine]]
