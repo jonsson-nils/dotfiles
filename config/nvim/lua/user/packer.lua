c9995092d06c780622b4c3a27500f197452066ed
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-05-09 10:06:39 +0200

    Replaced "" with ''.

--- config/nvim/lua/user/packer.lua
+++ config/nvim/lua/user/packer.lua
@@ -34,8 +34,8 @@ end)
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'folke/tokyonight.nvim'
-  use "folke/zen-mode.nvim"
-  use "folke/twilight.nvim"
+  use 'folke/zen-mode.nvim'
+  use 'folke/twilight.nvim'
   use 'folke/which-key.nvim'
   use 'preservim/nerdcommenter'
   use 'lukas-reineke/indent-blankline.nvim'
@@ -139,37 +139,37 @@ return require('packer').startup(function(use)
   use 'tpope/vim-repeat'
 
   use {
-    "nvim-neorg/neorg",
+    'nvim-neorg/neorg',
     config = function()
       require('neorg').setup {
         load = {
-          ["core.defaults"] = {},  -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = {      -- Manages Neorg workspaces
+          ['core.defaults'] = {},  -- Loads default behaviour
+          ['core.concealer'] = {}, -- Adds pretty icons to your documents
+          ['core.dirman'] = {      -- Manages Neorg workspaces
             config = {
               workspaces = {
-                notes = "~/notes",
+                notes = '~/notes',
               },
             },
           },
         },
       }
     end,
-    run = ":Neorg sync-parsers",
-    requires = "nvim-lua/plenary.nvim",
+    run = ':Neorg sync-parsers',
+    requires = 'nvim-lua/plenary.nvim',
   }
   use {
-    "ThePrimeagen/refactoring.nvim",
+    'ThePrimeagen/refactoring.nvim',
     requires = {
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-treesitter/nvim-treesitter" }
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-treesitter/nvim-treesitter' }
     }
   }
   use({
-    "kylechui/nvim-surround",
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    'kylechui/nvim-surround',
+    tag = '*', -- Use for stability; omit to use `main` branch for the latest features
     config = function()
-      require("nvim-surround").setup({})
+      require('nvim-surround').setup({})
     end
   })
   use { 'ThePrimeagen/git-worktree.nvim',
@@ -186,45 +186,45 @@ return require('packer').startup(function(use)
     end
   }
   use {
-    "iamcco/markdown-preview.nvim",
-    run = function() vim.fn["mkdp#util#install"]() end,
+    'iamcco/markdown-preview.nvim',
+    run = function() vim.fn['mkdp#util#install']() end,
     setup = function()
     end,
-    ft = { "markdown" },
+    ft = { 'markdown' },
   }
   use {
-    "folke/trouble.nvim",
-    requires = "nvim-tree/nvim-web-devicons",
+    'folke/trouble.nvim',
+    requires = 'nvim-tree/nvim-web-devicons',
   }
   use {
-    "folke/todo-comments.nvim",
-    requires = "nvim-lua/plenary.nvim",
+    'folke/todo-comments.nvim',
+    requires = 'nvim-lua/plenary.nvim',
   }
   use({
-    "folke/noice.nvim",
+    'folke/noice.nvim',
     requires = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
     }
   })
   use({
-    "folke/persistence.nvim",
-    event = "BufReadPre", -- this will only start session saving when an actual file was opened
-    module = "persistence",
+    'folke/persistence.nvim',
+    event = 'BufReadPre', -- this will only start session saving when an actual file was opened
+    module = 'persistence',
   })
   use {
-    "windwp/nvim-autopairs",
+    'windwp/nvim-autopairs',
     config = function()
-      require("nvim-autopairs").setup({
-        disable_filetype = { "TelescopePrompt", "vim" },
+      require('nvim-autopairs').setup({
+        disable_filetype = { 'TelescopePrompt', 'vim' },
       })
     end,
   }
   use {
-    "startup-nvim/startup.nvim",
-    requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    'startup-nvim/startup.nvim',
+    requires = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
     config = function()
-      require "startup".setup({ theme = 'evil' })
+      require 'startup'.setup({ theme = 'evil' })
     end
   }
 
