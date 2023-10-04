b8bcb114e10a7e01e8c9cd6afbe3f739bdcd3800
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-10-05 01:25:21 +0200

    Fixed some issues with neovim.

--- config/nvim/after/plugin/indent_blankline.lua
+++ config/nvim/after/plugin/indent_blankline.lua
@@ -1,7 +1,3 @@
-require("indent_blankline").setup {
-  -- for example, context is off by default, use this to turn it on
-  show_current_context = true,
-  show_current_context_start = false,
-}
+require("ibl").setup()
 
 vim.cmd [[highlight IndentBlanklineContextChar guifg=#FFFFFF gui=nocombine]]
