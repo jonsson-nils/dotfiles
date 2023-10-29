6b257501dc291c1e6a95e21e80b31d4241b8ed71
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-10-29 02:09:42 +0200

    Implemented starting nvim from broot.

--- bin/broot-nvim.sh
+++ bin/broot-nvim.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-if [ -e './.nvim.pipe' ]; then
-  nvim --server ./.nvim.pipe --remote "$2"
-  nvim --server ./.nvim.pipe --remote-send "<C-\\><C-N>$1gg<CR>"
-else
-  nvim "$1" "$2"
+echo "$PWD"
+
+if [ ! -e './.nvim.pipe' ]; then
+  echo "No .nvim.pipe file found."
+  tmux split-window "nvim --listen ./.nvim.pipe"
+  sleep 1
 fi
+
+nvim --server ./.nvim.pipe --remote "$2"
+nvim --server ./.nvim.pipe --remote-send "<C-\\><C-N>$1gg<CR>"
 
